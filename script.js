2d07692f9f7393fc8d453d7e604da63a61619334
hamza1851 <hamza.dev011@gmail.com>
2024-10-21 00:31:17 +0530

    Fixed heights

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const hamburgerIcon = document.querySelector(".hamburger--icon")
 const drawer = document.querySelector(".drawer")
+const overlay = document.createElement("div")
+overlay.classList.add("overlay")
 
 hamburgerIcon.addEventListener("click", (event) => {
   drawer.style.left = "0"
+
+  // document.body.style.overflow = "hidden"
+  document.body.scrollTo(0, 0)
 })
 
 document.addEventListener("click", (event) => {
   if (!drawer.contains(event.target) && !hamburgerIcon.contains(event.target)) {
     drawer.style.left = "-150%"
+
+    // document.body.style.overflow = "auto"
   }
 })
+
+
